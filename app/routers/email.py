768382e09f153df22cc4ddb2dbf82da4ac36f6f4
Karthi <karthikthee7@gmail.com>
2024-08-18 21:35:11 +0530

    new update in email

--- app/routers/email.py
+++ app/routers/email.py
@@ -24,25 +24,63 @@ router = APIRouter(prefix="/email", tags=["E-Mail"])
 db_dep = Annotated[AsyncSession, Depends(get_async_db)]
 user_dep = Annotated[dict, Depends(get_current_user)]
 
+email_cc = ['mahendran@trustingroup.in' , 'benzimen@trustingroup.in']
+
+darft_subject = "Draft test report for conformation"
+darft_msg = """
+Dear Sir/Madam,
+ 
+Greetings from Trustin Analytical Solutions…!!
+ 
+Please find the attached draft report for your review and conformation, kindly confirm to proceed further.
+
+
+"""
+subject = "Final test report"
+message = """
+Dear Sir/Madam,
+
+Greetings from Trustin Analytical Solutions…!!
+
+Please find the attached signed copy of test report for your reference.
+"""
+html_message = """
+<html>
+<body>
+   <p style="color:"red";"}>Note: Kindly respond within three days, otherwise this report will be considered as the final.<p>
+
+</body>
+</html>
+"""
 
 def send_email(email: EmailSchema):
     try:
         msg = MIMEMultipart()
         msg["From"] = FROM_EMAIL
         msg["To"] = email.email
-        msg["Subject"] = email.subject
-        msg.attach(MIMEText(email.message, "plain"))
+        msg["Cc"] = ','.join(email_cc)
+
+        if email.email_type == "DRAFT":
+            msg["Subject"] = darft_subject
+            msg.attach(MIMEText(darft_msg, "plain"))
+            msg.attach(MIMEText(html_message, "html"))
+        else:
+            msg["Subject"] = subject
+            msg.attach(MIMEText(message, "plain"))
 
         if email.attachment:
             pdf_blob = base64.b64decode(email.attachment)
             part = MIMEApplication(pdf_blob, Name=email.filename)
             part["Content-Disposition"] = f'attachment; filename="{email.filename}"'
             msg.attach(part)
+        
+        recipients = [email.email] + (email_cc if email_cc else [])
+
 
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SMTP_USERNAME, SMTP_PASSWORD)
-            server.send_message(msg)
+            server.send_message(msg, from_addr=FROM_EMAIL, to_addrs=recipients,)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to send email: {str(e)}")
--- app/schemas/email.py
+++ app/schemas/email.py
@@ -4,7 +4,8 @@ from pydantic import BaseModel, EmailStr
 
 class EmailSchema(BaseModel):
     email: EmailStr
-    subject: str
-    message: str
+    # subject: str
+    # message: str
+    email_type: str
     attachment: Optional[str] = None
     filename:  Optional[str] = None
